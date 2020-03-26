cbe1fa0e411e6214aaa1a72cfff1541d6554ead0
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-03-25 21:10:23 -0300

    Front-end do app

--- Backend/src/controllers/incidentController.js
+++ Backend/src/controllers/incidentController.js
@@ -10,7 +10,14 @@ module.exports = {
          .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
          .limit(5)
          .offset((page - 1) * 5)
-         .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+         .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+         ]);
 
       response.header('X-Total-Count', count['count(*)']);
 
@@ -39,8 +46,6 @@ module.exports = {
          .where('id', id)
          .select('ong_id')
          .first();
-
-      console.log(incident);
 
       if (incident.ong_id != ong_id) {
          return response.status(401).json({ error: 'Operation not allowed.' });
